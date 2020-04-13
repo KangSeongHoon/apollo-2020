4a500baec3a2b36c000ffdd62af7d92ff38a3b58
unknown <adept6646@naver.com>
2020-04-13 15:28:02 +0900

    apollo-2020

---
+++ src/apollo.js
@@ -0,0 +1,27 @@
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+    uri: "http://localhost:4000",
+    resolvers: {
+        Movie: {
+            isLiked: () => false
+        },
+        Mutation: {
+            toggleLikeMovie: (_, { id, isLiked }, { cache }) => {
+                cache.writeData({
+                    id: `Movie:${id}`,
+                    data: {
+                        isLiked: !isLiked
+                    }
+                });
+            },
+            unlikeMovie: (_, { id }, { cache }) => {
+                cache.writeData({
+
+                })
+            }
+        }
+    }
+});
+
+export default client;
